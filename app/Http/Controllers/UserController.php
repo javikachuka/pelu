00a20d90ef1f97d53aa5eb46486d0172edca523a
Agus <agus.britez@hotmail.com>
2020-04-22 21:16:21 -0300

    Usuario validacion

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,35 +3,58 @@
 namespace App\Http\Controllers;
 
 use App\User;
+use Carbon\Carbon;
+use Carbon\Traits\Date;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index(){
-        $users = User::all() ;
-        return view('users.index' , compact('users')) ;
+    public function index()
+    {
+        $users = User::all();
+        return view('users.index', compact('users'));
     }
 
-    public function create(){
-        $users = User::all() ;
-        return view('users.create') ;
+    public function create()
+    {
+        $users = User::all();
+        return view('users.create');
     }
 
 
-    public function save(Request $request){
+    public function save(Request $request)
+    {
 
 
-        $user = new User() ;
+        $this->validacionUsuariosEntrada($request);
+
+        $user = new User();
         $user->name = $request->name;
         $user->apellido = $request->apellido;
         $user->dni = $request->dni;
         $user->fecha_nacimiento = $request->fecha_nacimiento;
         $user->email = $request->email;
         $user->password = Hash::make($request->password);
-        $user->save() ;
+        $user->save();
         return redirect()->route('users.index');
-
     }
 
+    private function validacionUsuariosEntrada(Request $request)
+    {
+        $rules = [
+            'name'    =>  'required|max:255',
+            'apellido'    =>  'required|max:255',
+            'dni'    =>  'required|integer',
+            'fecha_nacimiento'    =>  'required|before:today',
+            'email'    =>  'required|unique:users',
+            'password'    =>  'required',
+        ];
+
+        $mensaje = [
+            'fecha_nacimiento.before' => 'La fecha de nacimiento tiene que ser menor al ' . Date('d/m/Y')
+        ];
+
+        $this->validate($request, $rules, $mensaje);
+    }
 }
--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -8,34 +8,35 @@
             <li>Recuerde que puede crear mas de un horario, es por eso que se solicita dar un nombre al horario</li>
         </ul>
     </div>
-    <form class="form-group " method="POST" action="{{route("users.save")}}" >
-    <div class="card-body">
-        <div class="form-group">
-            <label for="nombre">Nombre</label>
-            <input type="text" class="form-control" id="nombre" name="name" required>
+    @include('messageError')
+    <form class="form-group " method="POST" action="{{route("users.save")}}">
+        <div class="card-body">
+            <div class="form-group">
+                <label for="nombre">Nombre</label>
+                <input type="text" class="form-control" id="nombre" name="name" required>
+            </div>
+            <div class="form-group">
+                <label for="apellido">Apellido</label>
+                <input type="text" class="form-control" id="apellido" name="apellido" required>
+            </div>
+            <div class="form-group">
+                <label for="dni">DNI</label>
+                <input type="text" class="form-control" id="dni" name="dni" required>
+            </div>
+            <div class="form-group">
+                <label for="fecha_nacimiento">Fecha de Nacimiento</label>
+                <input type="date" class="form-control" id="fecha_nacimiento" name="fecha_nacimiento" required>
+            </div>
+            <div class="form-group">
+                <label>Email</label>
+                <input type="text" name="email" required class="form-control" value="">
+            </div>
+            <div class="form-group">
+                <label>Contraseña</label>
+                <input type="password" name="password" class="form-control" value="" required>
+            </div>
         </div>
-        <div class="form-group">
-            <label for="apellido">Apellido</label>
-            <input type="text" class="form-control" id="apellido" name="apellido" required>
-        </div>
-        <div class="form-group">
-            <label for="dni">DNI</label>
-            <input type="text" class="form-control" id="dni" name="dni" required>
-        </div>
-        <div class="form-group">
-            <label for="fecha_nacimiento">Fecha de Nacimiento</label>
-            <input type="date" class="form-control" id="fecha_nacimiento" name="fecha_nacimiento" required>
-        </div>
-        <div class="form-group">
-            <label>Email</label>
-            <input type="text" name="email" required class="form-control" value="">
-        </div>
-        <div class="form-group">
-            <label>Contraseña</label>
-            <input type="password" name="password" class="form-control" value="" required>
-        </div>
-    </div>
-    <button type="submit" class="btn btn-secondary">Crear</button>
+        <button type="submit" class="btn btn-secondary">Crear</button>
         @csrf
     </form>
 
