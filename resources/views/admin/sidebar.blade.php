cb67069f4fbf79f12d71956bdd5e8d28dcb55a25
Javier <kachuka99@gmail.com>
2020-07-25 16:00:54 -0300

    Cambios en el sidebar
    Reposicionamiento de items del sidebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -4,21 +4,13 @@
     <a href="index3.html" class="brand-link">
         <img src="{{asset('admin-lte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo"
             class="brand-image img-circle elevation-3" style="opacity: .8">
-        <span class="brand-text font-weight-light">PeluPapa</span>
+        <span class="brand-text font-weight-light">{{auth()->user()->empresa->nombre}}</span>
     </a>
 
     <!-- Sidebar -->
     <div class="sidebar">
         <!-- Sidebar user panel (optional) -->
-        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
-            <div class="image">
-                <img src="{{ asset('admin-lte/dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2"
-                    alt="User Image">
-            </div>
-            <div class="info">
-                <a href="#" class="d-block">Usuario</a>
-            </div>
-        </div>
+
 
         <!-- Sidebar Menu -->
         <nav class="mt-2">
@@ -27,11 +19,25 @@
                 <!-- Add icons to the links using the .nav-icon class
        with font-awesome or any other icon font library -->
                 <li class="nav-item">
+                    <a href="{{route('empresas.index',auth()->user()->empresa->slug)}}"
+                        class="nav-link  {{ (request()->routeIs('empresas.*')) ? 'active' : '' }}">
+                        <i class="fal fa-home nav-icon"></i>
+                        <p>Mi Inicio</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route('misTurnos.misTurnos')}}"
+                        class="nav-link  {{ (request()->routeIs('misTurnos.*')) ? 'active' : '' }}">
+                        <i class="fal fa-circle-notch nav-icon"></i>
+                        <p>Mis turnos en esperas</p>
+                    </a>
+                </li>
+                {{-- <li class="nav-item">
                     <a href="{{route('home')}}" class="nav-link {{ (request()->is('home')) ? 'active' : '' }}">
                         <i class="fal fa-home nav-icon"></i>
                         <p>Home</p>
                     </a>
-                </li>
+                </li> --}}
                 <li class="nav-item">
                     <a href="{{route('turnos.index')}}"
                         class="nav-link  {{ (request()->routeIs('turnos.*')) ? 'active' : '' }}">
@@ -60,20 +66,7 @@
                         <p>Horarios</p>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a href="{{route('empresas.index',auth()->user()->empresa->slug)}}"
-                        class="nav-link  {{ (request()->routeIs('empresas.*')) ? 'active' : '' }}">
-                        <i class="fal fa-circle-notch nav-icon"></i>
-                        <p>Mi Inicio</p>
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a href="{{route('turnos.misTurnos')}}"
-                        class="nav-link  {{ (request()->routeIs('turnos.*')) ? 'active' : '' }}">
-                        <i class="fal fa-circle-notch nav-icon"></i>
-                        <p>Mis turnos en esperas</p>
-                    </a>
-                </li>
+
 
                 {{-- Listado desplegable para agrupar elementos --}}
                 {{-- <li class="nav-item has-treeview menu-open">
